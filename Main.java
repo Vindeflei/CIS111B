fb142a63d3c13a9e99fa016319adbfaccaf42a67
Vindeflei <audrey.d.weidner@gmail.com>
2023-11-13 19:23:13 -0500

    KAN-19: Basic GitHub setup

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main{
+
+   
+   public static void main(String[] args) throws Exception {
+      //The beginning of something great!
+   }
+
+
+
+
+
+}

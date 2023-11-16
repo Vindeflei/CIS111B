42247bc0ac22b6c15590b2761468d532fcd74b44
Vindeflei <audrey.d.weidner@gmail.com>
2023-11-16 17:43:43 -0500

    KAN-25

--- Region.java
+++ Region.java
@@ -1,11 +1,17 @@
 
 public class Region{
    private String name;
-   private Dex[] pokedexes;
-}
-public String getName(){
-   return name;
-}
-public Dex[] getDexes(){
-   return pokedexes;
+   private NameValue[] pokedexes;
+   public String getName(){
+      return name;
+   }
+   public String[] getDexes(){
+      String[] temp = new String[pokedexes.length];
+      int i = 0;
+      for(NameValue d:pokedexes){
+         temp[i]=d.getName();
+         i++;
+      }
+      return temp;
+   }
 }

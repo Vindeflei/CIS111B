23eb045ee792312492262abd64e4757d698cd453
Vindeflei <audrey.d.weidner@gmail.com>
2023-11-16 17:41:15 -0500

    KAN-27

---
+++ PokemonSprites.java
@@ -0,0 +1,11 @@
+
+public class PokemonSprites{
+   private String name;
+   private SpriteList sprites;
+   public String getName(){
+      return name;
+   }
+   public String[] getSprites(){
+      return this.sprites.getSprites();
+   }
+}

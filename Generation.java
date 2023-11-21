54995a8c9d4221264293e82cefe0be8336d98049
Vindeflei <audrey.d.weidner@gmail.com>
2023-11-21 12:37:23 -0500

    KAN-26

---
+++ Generation.java
@@ -0,0 +1,54 @@
+/**
+*A class for holding generation data from the API.
+*Built for use with GSON.
+*
+*@author Cat Weidner
+*/
+public class Generation{
+   /**
+   *The ID name the API uses
+   */
+   private String name;
+   /**
+   *An array of the names for this generation
+   *in various languages
+   */
+   private NameValue[] names;
+   /**
+   *An array of the names of the pokemon in
+   *this generation
+   */
+   private NameValue[] pokemon_species;
+   /**
+   *Returns the ID name used by the API
+   *
+   *@return ID name
+   */
+   public String getIDName(){
+      return name;
+   }
+   /**
+   *Returns the display name of the generation,
+   *in English
+   *
+   *@return display name
+   */
+   public String getDisplayName(){
+      return names[6].getName(); 
+   }
+   /**
+   *Returns a list of the names of the pokemon
+   *in this generation
+   *
+   *@return list of pokemon names
+   */
+   public String[] getPokeList{
+      String[] temp;
+      int i = 0;
+      for(NameValue n:pokemon_species){
+         temp[i] = n.getName();
+         i++;
+      }
+      return temp;
+   }
+}

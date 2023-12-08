b273d6afc5abbe495569be72c4f480b43e2b9998
Vindeflei <audrey.d.weidner@gmail.com>
2023-12-08 17:27:19 -0500

    Basic launch!

--- PokeAppMain.java
+++ PokeAppMain.java
@@ -16,24 +16,16 @@ public class PokeAppMain extends Application
    public void start(Stage stage) throws Exception
    {
       // Load the GUI from FXML built in Scene Builder
-      FXMLLoader loader = new FXMLLoader();
-      final URL resource = getClass().getResource("PokeApp.fxml");
-      loader.setLocation(resource);
-      System.out.println(loader.getLocation());
-      Parent root = loader.load(resource);
-      //Parent root = FXMLLoader.load(getClass().getResource("PokeApp.fxml"));    
-      // Scene scene = new Scene(root);      
-//       stage.setTitle("PokePics App");
-//       
-//       // The stylesheet is currently set in Scene Builder
-//       // Note you can also load this programmatically
-//       // scene.getStylesheets().add("styles.css");
-// 
-//       stage.setScene(scene);
-//       stage.show();
-//       
-      // All of the code that interacts with the API and the Scene 
-      //   is found in FXMLTempAppController.java
+      Parent root = FXMLLoader.load(getClass().getResource("PokeApp.fxml"));
+      Scene scene = new Scene(root);      
+      stage.setTitle("PokePics App");
+      
+      // The stylesheet is currently set in Scene Builder
+      // Note you can also load this programmatically
+      // scene.getStylesheets().add("styles.css");
+
+      stage.setScene(scene);
+      stage.show();
       
    }
    @Override
